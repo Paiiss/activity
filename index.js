4ab6a06bb4b984534290d81884445c3b859a9b13
Paiiss <pais.2k20@gmail.com>
2023-08-30 00:59:02 +0700

    Update

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const { Client, RichPresence } = require('discord.js-selfbot-v13');
-const Discord = require('discord.js-selfbot-v13');
+const config = require('./config.json')
 const dotenv = require('dotenv')
 dotenv.config()
+
 const client = new Client({
-    // See other options here
     checkUpdate: false,
-    // https://discordjs-self-v13.netlify.app/#/docs/docs/main/typedef/ClientOptions
-    // All partials are loaded automatically
 });
 
 client.on('ready', async () => {
     console.log(`${client.user.username} is ready!`);
     const r = new RichPresence()
-        .setApplicationId('817229550684471297')
-        .setType('PLAYING')
-        .setURL('https://gas138divine.com/account/register/bostolan')
-        // .setState('State')
-        .setName('Gate Of Olympus')
-        .setDetails('Agar tali saliturahmi tidak terputus')
-        .setState('adakah seratus?!')
-        // .setParty({
-        //     max: 9,
-        //     current: 1,
-        //     id: Discord.getUUID(),
-        // })
+        .setApplicationId(config.appId)
+        .setType(config.type)
+        .setURL(config.url)
+        .setName(config.name)
+        .setDetails(config.details)
+        .setState(config.state)
         .setStartTimestamp(Date.now())
-        .setAssetsLargeImage('mp:attachments/1061738701140537364/1145928780372770816/Z.png')
-        .setAssetsLargeText('Gate Of Olympus')
-        .setAssetsSmallImage('mp:attachments/1061738701140537364/1145928780372770816/Z.png')
-        .setAssetsSmallText('gas138')
-        .addButton('Hayu depo', 'https://gas138divine.com/account/register/bostolan')
+        .setAssetsLargeImage(config.image)
+        .setAssetsLargeText(config.largeText)
+        .setAssetsSmallImage(config.imageSmall)
+        .setAssetsSmallText(config.smallText)
+        .addButton(config.buttonText, config.url)
     client.user.setActivity(r);
 })
 
